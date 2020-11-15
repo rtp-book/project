1be5fa6104e49cbeb94732ef54237c73d2032696
John Sheehan <jennasyseng@gmail.com>
2020-11-14 16:23:19 -0800

    Added step11 files

---
+++ client/src/views/bookList/bookListFilter.py
@@ -0,0 +1,113 @@
+from common.pyreact import useState, createElement as el
+from common.pymui import TextField, Button, Paper
+from main.appTheme import Flexbox
+
+
+def CategoriesList(props):
+    categories = props['categories']
+
+    def categoryToRow(author):
+        category_id = author['ID']
+        category_name = author['Category']
+
+        return el('option', {'key': category_id, 
+                             'value': category_name
+                            }, category_name)
+
+    return [categoryToRow(category) for category in categories]
+
+
+def BooksFilterVu(props):
+    categories = props['categories']
+    setFilterParams = props['setFilterParams']
+
+    Title, setTitle = useState("")
+    Author, setAuthor = useState("")
+    IsFiction, setIsFiction = useState("")
+    Category, setCategory = useState("")
+    ISBN, setISBN = useState("")
+
+    def setState(field, value):
+        switch = dict(Title=setTitle,
+                      Author=setAuthor,
+                      IsFiction=setIsFiction,
+                      Category=setCategory,
+                      ISBN=setISBN
+                     )
+        switch[field](value)
+
+    def handleInputChange(event):
+        event.preventDefault()
+        target = event['target']
+        value = target['value']
+        key = target['name']
+        setState(key, value)
+
+    def handleFilter():
+        params = dict(Title=Title,
+                      Author=Author,
+                      IsFiction=IsFiction,
+                      Category=Category,
+                      ISBN=ISBN
+                     )
+        filters = {key: val for key, val in params.items() if len(val) > 0}
+        setFilterParams(filters)
+
+    filter_width = '17%'
+
+    return el(Paper, None,
+              el(Flexbox, {'flexWrap': 'wrap', 'style': {'margin': '0.5rem'}},
+                 el(TextField, {'label': "Title",
+                                'name': 'Title',
+                                'style': {'width': filter_width},
+                                'value': Title,
+                                'onChange': handleInputChange,
+                               }
+                   ),
+                 el(TextField, {'label': "Author",
+                                'name': 'Author',
+                                'style': {'width': filter_width},
+                                'value': Author,
+                                'onChange': handleInputChange,
+                               }
+                   ),
+                 el(TextField, {'label': "Genre",
+                                'name': 'IsFiction',
+                                'style': {'width': filter_width},
+                                'value': IsFiction,
+                                'onChange': handleInputChange,
+                                'select': True,
+                                'SelectProps': {'native': True},
+                               },
+                    el('option', {'value': ''}, ""),
+                    el('option', {'value': '1'}, "Fiction"),
+                    el('option', {'value': '0'}, "Non-Fiction"),
+                   ),
+                 el(TextField, {'label': "Category",
+                                'name': 'Category',
+                                'style': {'width': filter_width},
+                                'value': Category,
+                                'onChange': handleInputChange,
+                                'select': True,
+                                'SelectProps': {'native': True},
+                               },
+                    el('option', {'value': ''}),
+                    el(CategoriesList, {'categories': categories}),
+                   ),
+                 el(TextField, {'label': "ISBN",
+                                'name': 'ISBN',
+                                'style': {'width': filter_width},
+                                'value': ISBN,
+                                'onChange': handleInputChange,
+                               }
+                   ),
+                 el(Button, {'type': 'button',
+                             'color': 'primary',
+                             'size': 'small',
+                             'style': {'minWidth': '7rem', 'margin': '0.5rem'},
+                             'onClick': handleFilter
+                            }, "Filter"
+                   ),
+                )
+             )
+
--- client/src/views/bookList/bookListView.py
+++ client/src/views/bookList/bookListView.py
@@ -2,6 +2,7 @@ from common.pyreact import useState, useEffect, createElement as el
 from common.pymui import Typography, AppBar, Toolbar, IconButton, CloseIcon
 from common.pymui import Container, Box, Paper, CircularProgress
 from common.urlutils import fetch
+from views.bookList.bookListFilter import BooksFilterVu
 from views.bookList.bookListTable import BooksTable
 
 
@@ -11,6 +12,12 @@ def BookList(props):
     books, setBooks = useState([])
     sortKey, setSortKey = useState('Title')
     showProgress, setShowProgress = useState(False)
+    filterParams, setFilterParams = useState({})
+
+    categories, setCategories = useState([])
+    publishers, setPublishers = useState([])
+    formats, setFormats = useState([])
+    conditions, setConditions = useState([])
 
     def sortBooks():
         book_list = [dict(tmp_book) for tmp_book in books]
@@ -36,11 +43,38 @@ def BookList(props):
             getBooks.isPending = False
 
         setShowProgress(True)
-        fetch("/api/books", _getBooks, onError=on_fetch_error)
+        fetch("/api/books", _getBooks,
+              params=filterParams,
+              onError=on_fetch_error
+             )
         return abort
 
-    useEffect(getBooks, [])
+    def getLookup(table_name, setState):
+        getLookup.isPending = True
+
+        def _getLookup(data):
+            if getLookup.isPending:
+                if data:
+                    setState(data)
+                else:
+                    setState([])
+
+        def abort():
+            getLookup.isPending = False
+
+        fetch(f"/api/lookup/{table_name}", _getLookup)
+        return abort
+
+    def getLookups():
+        getLookup('Categories', setCategories)
+        getLookup('Publishers', setPublishers)
+        getLookup('Formats', setFormats)
+        getLookup('Conditions', setConditions)
+
+    useEffect(getLookups, [])
+    useEffect(getBooks, [filterParams])
     useEffect(sortBooks, [sortKey])
+
 
     return el(Container, None,
               el(AppBar, {'position': 'static',
@@ -56,6 +90,9 @@ def BookList(props):
                                    }, el(CloseIcon, None)
                       ),
                    ),
+                ),
+              el(BooksFilterVu, {'categories': categories,
+                                 'setFilterParams': setFilterParams}
                 ),
               el(Paper, {'style': {'padding': '0.5rem', 'marginTop': '0.8rem'}},
                  el(BooksTable, {'books': books, 'setSortKey': setSortKey})
